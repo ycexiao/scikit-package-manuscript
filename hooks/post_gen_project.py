1fd30af53cac94c9d3563e4fe442dd8147b09248
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-05-28 14:42:26 -0400

    fix: replace 'repo_name' with 'project_slug'

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -70,7 +70,7 @@ def load_template(source_dir, target_dir):
 
 def main():
     target_directory = Path().cwd()
-    copy_package_files("bg-cookiecutter-overleaf.templates", cookiecutter.template, target_directory)
+    copy_package_files("scikit-package-manuscript.templates", "{{ cookiecutter.template }}", target_directory)
     clone_headers(target_directory)
     template_directory = Path().cwd() / cookiecutter.template
     load_template(template_directory, target_directory)
