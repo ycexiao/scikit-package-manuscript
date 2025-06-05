8e3a0bfc2b545af38f877105c190598f2e6c875d
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-06-05 14:29:54 -0400

    refactor: define a variable for `manuscript.tex`

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -44,7 +44,6 @@ def clone_headers(repo_url):
     with tempfile.TemporaryDirectory() as tmp:
         tmp_path = Path(tmp)
         subprocess.run(["git", "clone", repo_url, str(tmp_path)], check=True)
-
         for item in tmp_path.glob('**/*'):
             if item.is_file() and str(item).endswith(".tex"):
                 headers += item.read_text()+'\n'
@@ -59,46 +58,39 @@ def extract_blocks(header_str):
     keyword_pattern = re.compile(r'^\\([a-zA-Z]+)(?=\{|\[)')
     name_pattern = re.compile(r'(?<=\{)(.*?)(?=\})')
     blocks = []
-
     open_braces = 0
     next_line_same_block = False
 
     for i, line in enumerate(lines):
         if line.strip().startswith('%') or not line.strip():
             continue
-
         else:
             if next_line_same_block:
                 blocks[-1]['content'] += line.rstrip() + '\n'
                 open_braces += line.count('{') - line.count('}')
-
             else:
                 keywoard_match = keyword_pattern.findall(line)
                 name_match = name_pattern.findall(line)
                 if not keywoard_match or not name_match:
                     raise ValueError(f"Invalid block header: {line}")
-                
                 block = {}
                 block['content'] = line.strip() + '\n'
                 block['keyword'] = keywoard_match[0].strip()
                 block['name'] = name_match[0].strip()
                 blocks.append(block)
-
                 open_braces += line.count('{') - line.count('}')
-
             if open_braces != 0:
                 next_line_same_block = True
             else:
                 next_line_same_block = False
     return blocks
 
-def sort_blocsks(blocks):
+def sort_blocks(blocks):
     """
     Classify extracted blocks into package and command blocks and sort each type of the blocks alpha-numerically by their names.
     """
     package_keyword = ['usepackage']
     command_keyword = ['newcommand', 'renewcommand', 'providecommand']
-
     package_blocks = []
     command_blocks = []
 
@@ -116,11 +108,11 @@ def sort_blocsks(blocks):
     return package_blocks, command_blocks
     
     
-def insert_blocks(target_dir, package_blocks, command_blocks):
+def insert_blocks(target_dir, target_file, package_blocks, command_blocks):
     """
     Inserts LaTeX header blocks into the manuscript.tex file in the target directory after the \\documentclass line.
     """
-    target_path = Path(target_dir) / "manuscript.tex"
+    target_path = Path(target_dir) / target_file
     before_insert_region = ""
     after_insert_region = ""
     before_insert = True
@@ -131,7 +123,6 @@ def insert_blocks(target_dir, package_blocks, command_blocks):
                 before_insert_region += line
             else:
                 after_insert_region += line
-
             if len(line)>= len(insert_start) and line[:len(insert_start)] == insert_start:
                 before_insert = False
                 before_insert_region += "\n% Start of inserted headers\n"
@@ -142,7 +133,6 @@ def insert_blocks(target_dir, package_blocks, command_blocks):
         headers_content += block['content'] 
     for block in command_blocks:
         headers_content += block['content']
-    
     new_content = before_insert_region + headers_content + after_insert_region
 
     with open(target_path, 'w') as f:
@@ -158,8 +148,9 @@ def main():
     copy_package_files("{{ cookiecutter.journal_template }}", target_directory)
     headers = clone_headers("{{ cookiecutter.latex_headers_repo }}")
     header_blocks = extract_blocks(headers)
-    package_blocks, command_blocks = sort_blocsks(header_blocks)
-    insert_blocks(target_directory , package_blocks, command_blocks)
+    package_blocks, command_blocks = sort_blocks(header_blocks)
+    target_file = "manuscript.tex"
+    insert_blocks(target_directory, target_file , package_blocks, command_blocks)
 
 if __name__ == "__main__":
     main()
