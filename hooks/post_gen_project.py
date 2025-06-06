56351b503f6032799d63ef76a9d9eb7326468455
Simon Billinge <sbillinge@users.noreply.github.com>
2025-06-05 22:20:35 -0400

    fix: global variable in caps

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,7 @@ from importlib.resources import as_file, files
 import sys
 import re
 
-main_tex_file = "manuscript.tex"
+MANUSCRIPT_FILENAME = "manuscript.tex"
 
 
 def copy_package_files(resource_dir: str, target_dir: Path):
@@ -149,7 +149,7 @@ def main():
     headers = clone_headers("{{ cookiecutter.latex_headers_repo }}")
     header_blocks = extract_blocks(headers)
     package_blocks, command_blocks = sort_blocks(header_blocks)
-    target_file = target_directory / main_tex_file
+    target_file = target_directory / MANUSCRIPT_FILENAME
     insert_blocks(target_file, package_blocks, command_blocks)
 
 if __name__ == "__main__":
