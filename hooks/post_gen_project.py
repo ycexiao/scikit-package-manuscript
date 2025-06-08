8451560539c4989541eca1b45c63b3b3f752cffb
Simon Billinge <simon.billinge@gmail.com>
2025-06-08 10:01:45 -0400

    chore: remove old ethan code

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,4 @@
 import json
-import re
 import shutil
 import subprocess
 import tempfile
@@ -97,108 +96,6 @@ def recompose_manuscript(manuscript_path, user_packages, user_commands):
     manuscript_path.write_text(
         manuscript_contents_with_header, encoding="utf-8"
     )
-
-
-def extract_blocks(header_str):
-    """
-    Extracts blocks from a LaTeX header string.
-    """
-    lines = header_str.splitlines()
-    keyword_pattern = re.compile(r'^\\([a-zA-Z]+)(?=\{|\[)')
-    name_pattern = re.compile(r'(?<=\{)(.*?)(?=\})')
-    blocks = []
-    open_braces = 0
-    next_line_same_block = False
-
-    for i, line in enumerate(lines):
-        if line.strip().startswith('%') or not line.strip():
-            continue
-        else:
-            if next_line_same_block:
-                blocks[-1]['content'] += line.rstrip() + '\n'
-                open_braces += line.count('{') - line.count('}')
-            else:
-                keywoard_match = keyword_pattern.findall(line)
-                name_match = name_pattern.findall(line)
-                if not keywoard_match or not name_match:
-                    raise ValueError(f"Invalid block header: {line}")
-                block = {}
-                block['content'] = line.strip() + '\n'
-                block['keyword'] = keywoard_match[0].strip()
-                block['name'] = name_match[0].strip()
-                blocks.append(block)
-                open_braces += line.count('{') - line.count('}')
-            if open_braces != 0:
-                next_line_same_block = True
-            else:
-                next_line_same_block = False
-    return blocks
-
-
-def sort_blocks(blocks):
-    """
-    Classify extracted blocks into package and command blocks
-
-    Sort each type of the blocks alpha-numerically by their names.
-    """
-    package_keyword = ['usepackage']
-    command_keyword = ['newcommand', 'renewcommand', 'providecommand']
-    package_blocks = []
-    command_blocks = []
-
-    for i in range(len(blocks)):
-        if blocks[i]['keyword'] in package_keyword:
-            package_blocks.append(blocks[i])
-        elif blocks[i]['keyword'] in command_keyword:
-            command_blocks.append(blocks[i])
-        else:
-            command_blocks.append(blocks[i])
-
-    package_blocks = sorted(package_blocks,
-                            key=lambda x: get_alphanum_key(x['name']))
-    command_blocks = sorted(command_blocks,
-                            key=lambda x: get_alphanum_key(x['name']))
-    return package_blocks, command_blocks
-
-
-def get_alphanum_key(name):
-    return [int(text) if text.isdigit() else text.lower()
-            for text in re.split('([0-9]+)', name)]
-
-
-def insert_blocks(manuscript_file, package_blocks, command_blocks):
-    """
-    Inserts LaTeX header blocks into the manuscript.tex file
-
-    Blocks are inserted after the \\documentclass line.
-    """
-    before_insert_region = ""
-    after_insert_region = ""
-    before_insert = True
-    insert_start = "\\documentclass"
-    with open(manuscript_file, 'r') as f:
-        for line in f:
-            if before_insert:
-                before_insert_region += line
-            else:
-                after_insert_region += line
-            if len(line) >= len(insert_start) \
-                    and line[:len(insert_start)] == insert_start:
-                before_insert = False
-                before_insert_region += "\n% Start of inserted headers\n"
-                after_insert_region += "% End of inserted headers\n"
-
-    headers_content = ""
-    for block in package_blocks:
-        headers_content += block['content']
-    for block in command_blocks:
-        headers_content += block['content']
-    new_content = before_insert_region + headers_content + after_insert_region
-
-    with open(manuscript_file, 'w') as f:
-        f.write(new_content)
-
-    print(f"Headers written to {manuscript_file}")
 
 
 def main():
