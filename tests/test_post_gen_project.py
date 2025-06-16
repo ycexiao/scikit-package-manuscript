9696b67747171afaa07f351d7b075ed1ce64ce87
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-06-16 17:16:29 -0400

    test: remove `monkeypatch` and use `pytest.mark.parametrize`

--- tests/test_post_gen_project.py
+++ tests/test_post_gen_project.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from pathlib import Path
 
@@ -9,40 +10,105 @@ from hooks.post_gen_project import (  # noqa: E402
     get_repo_dir,
 )
 
+@pytest.mark.parametrize("repos",[
+    # test get_repo_dir when "scikit-package-manuscript" repo exists.
+    # C1: with only "scikit-package-manuscript" repo
+    ("scikit-package-manuscript",),
+    # C2: with "scikit-package-manuscript" and other repos
+    ("scikit-package-manuscript", "foo", "bar"),
+]
+)
+def test_get_repo_dir(user_filesystem, repos):
+    home_path, ck_path, cwd_path = user_filesystem
+    skm_repo_path = home_path / ".cookiecutters" / "scikit-package-manuscript"
+    os.environ["HOME"] = str(home_path)
+    for repo in repos:
+        repo_path = ck_path / repo
+        repo_path.mkdir()
+    actual = get_repo_dir()
+    expected = skm_repo_path
+    assert expected == actual
 
-def test_get_repo_dir(monkeypatch, user_filesystem):
-    home_dir = user_filesystem[0]
-    monkeypatch.setenv("HOME", str(home_dir))
-    repo_path = home_dir / ".cookiecutters" / "scikit-package-manuscript"
 
-    assert repo_path == get_repo_dir()
-
-
-def test_without_repo_get_repo_dir(monkeypatch, user_filesystem_without_repo):
-    home_dir = user_filesystem_without_repo[0]
-    monkeypatch.setenv("HOME", str(home_dir))
-    with pytest.raises(FileNotFoundError) as exc_info:
+@pytest.mark.parametrize("repos",[
+    # test get_repo_dir when "scikit-package-manuscript" repo does not exist.
+    # C1: empty ".cookiecutters"
+    (),
+    # C2: without "scikit-package-manuscript"
+    ("foo", "bar"),
+]
+)
+def test_get_repo_dir_bad(user_filesystem, repos):
+    home_path, ck_path, cwd_path = user_filesystem
+    os.environ["HOME"] = str(home_path)
+    for repo in repos:
+        repo_path = ck_path / repo
+        repo_path.mkdir()
+    with pytest.raises(FileNotFoundError, match=r"Couldn't find scikit-package-manuscript,") as exc_info:
         get_repo_dir()
-    assert "Couldn't find scikit-package-manuscript," in str(exc_info.value)
+
+@pytest.mark.parametrize("existing_templates, template",[
+    # test if copy_journal_template_files can find the expected template.
+    # C1: with only one template
+    (["article"], "article"),
+    # C2: with multiple templates
+    (["article", "foo", "bar"], "article")
+]
+)
+def test_copy_journal_template_files_find_template(user_filesystem_with_repo, existing_templates, template):
+    home_path, skm_repo_path, cwd_path = user_filesystem_with_repo
+    os.environ["HOME"] = str(home_path)
+    for existing_template in existing_templates:
+        existing_template_path = skm_repo_path / "templates" / existing_template
+        existing_template_path.mkdir(parents=True, exist_ok=True)
+    
+    cwd_path = copy_journal_template_files(template, cwd_path)
 
 
-def test_copy_journal_template_files(monkeypatch, user_filesystem):
-    home_dir = user_filesystem[0]
-    monkeypatch.setenv("HOME", str(home_dir))
-    journal_template = "article"
-    project_dir = user_filesystem[1]
-    project_dir = copy_journal_template_files(journal_template, project_dir)
-    manuscript_file = project_dir / "manuscript.tex"
-    assert manuscript_file.exists()
+@pytest.mark.parametrize("existing_templates, template",[
+    # test copy_journal_template_files when template is not existed.
+    # C1: empty templates.
+    ([], "article"),
+    # C2: template does not exist.
+    (["foo", "bar"], "article")
+]
+)
+def test_copy_journal_template_files_find_template_bad(user_filesystem_with_repo, existing_templates, template):
+    home_path, skm_repo_path, cwd_path = user_filesystem_with_repo
+    os.environ["HOME"] = str(home_path)
+    for existing_template in existing_templates:
+        existing_template_path = skm_repo_path / "templates" / existing_template
+        existing_template_path.mkdir(parents=True, exist_ok=True)
+
+    with pytest.raises(NotADirectoryError, match=r"Cannot find the provided journal_tamplate: ") as exc_info:
+        cwd_path = copy_journal_template_files(template, cwd_path)
 
 
-def test_without_template_copy_journal_template_files(
-    monkeypatch, user_filesystem_without_template
-):
-    home_dir = user_filesystem_without_template[0]
-    monkeypatch.setenv("HOME", str(home_dir))
-    journal_template = "article"
-    project_dir = user_filesystem_without_template[1]
-    with pytest.raises(NotADirectoryError) as exc_info:
-        project_dir = copy_journal_template_files(journal_template, project_dir)
-    assert "Cannot find the provided journal_tamplate: " in str(exc_info.value)
+@pytest.mark.parametrize("template_files",[
+    # test copy_journal_template_files with different directory structure.
+    # C1: empty template
+    (),
+    # C2: only one file in the template
+    ("manuscript.tex"),
+    # C3: multiple files in the template
+    ("manuscript.tex","texstyle.cls","bibstyle.bst"),
+    # C4: subdirectory in the template
+    ("manuscript.tex", "style/texstyle.cls", "style/bibstyle.bst")
+])
+def test_copy_journal_template_files_copy_files(user_filesystem_with_repo, template_files):
+    home_path, skm_repo_path, cwd_path = user_filesystem_with_repo
+    os.environ["HOME"] = str(home_path)
+    template_path = skm_repo_path/ "templates" / "article"
+    template_path.mkdir(parents=True, exist_ok=True)
+    for file in template_files:
+        file_path = template_path / file
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.touch()
+
+    project_path = cwd_path
+    project_path = copy_journal_template_files(template_path, project_path)
+
+    for file in template_files:
+        file_path = project_path / file
+        assert file_path.exists()
+
