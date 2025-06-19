735c1b1d1a533428c3309eb84a89bd9a23f3c2f1
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-06-19 15:12:14 -0400

    chore: fix pre-commit errors

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,11 +11,15 @@ def get_repo_dir():
     candidates = []
     for candidate in cookiecutters_dir.iterdir():
         candidates.append(candidate)
-        if (candidate.is_dir() and
-                "scikit-package-manuscript" in candidate.name):
+        if (
+            candidate.is_dir()
+            and "scikit-package-manuscript" in candidate.name
+        ):
             return candidate.resolve()
-    return Path(f"couldn't find scikit-package-manuscript, but did "
-                f"find {*candidates,}")  # noqa E231
+    return Path(
+        f"couldn't find scikit-package-manuscript, but did "
+        f"find {*candidates, }"
+    )  # noqa E231
 
 
 def copy_journal_template_files(journal_template, project_dir):
@@ -34,9 +38,11 @@ def copy_journal_template_files(journal_template, project_dir):
     cookiecutter_path = get_repo_dir()
     template_dir = cookiecutter_path / "templates" / journal_template
     if not template_dir.exists():
-        raise NotADirectoryError(f"Cannot find the provided journal_tamplate: "
-                                 f"{journal_template} in {template_dir}. "
-                                 f"Please contact the software developers")
+        raise NotADirectoryError(
+            f"Cannot find the provided journal_tamplate: "
+            f"{journal_template} in {template_dir}. "
+            f"Please contact the software developers"
+        )
     for item in template_dir.iterdir():
         dest = project_dir / item.name
         if item.is_dir():
@@ -54,20 +60,20 @@ def get_user_headers(repo_url):
     with tempfile.TemporaryDirectory() as tmp:
         tmp_path = Path(tmp)
         subprocess.run(["git", "clone", repo_url, str(tmp_path)], check=True)
-        for item in tmp_path.glob('**/*'):
+        for item in tmp_path.glob("**/*"):
             if item.is_file() and str(item).endswith(".tex"):
-                headers += item.read_text()+'\n'
+                headers += item.read_text() + "\n"
     return headers
 
 
-def extract_manuscript_keyword_lines(manuscript_path, keyword=r'\usepackage'):
+def extract_manuscript_keyword_lines(manuscript_path, keyword=r"\usepackage"):
     contents = manuscript_path.read_text(encoding="utf-8")
     packages, the_rest = split_keyword_lines(contents, keyword=keyword)
     Path(manuscript_path).write_text(the_rest, encoding="utf-8")
     return packages
 
 
-def split_keyword_lines(content, keyword=r'\usepackage'):
+def split_keyword_lines(content, keyword=r"\usepackage"):
     """Split the lines containing keyword from the content."""
     keyword_lines = []
     other_lines = []
@@ -79,18 +85,20 @@ def split_keyword_lines(content, keyword=r'\usepackage'):
     return "\n".join(keyword_lines), "\n".join(other_lines)
 
 
-def insert_keyword_lines(content, insert_text, location_keyword=r'\documentclass', method='below'):
-    """Inser the lines below or above a certain location in the content."""
+def insert_keyword_lines(
+    content, insert_text, location_keyword=r"\documentclass", method="below"
+):
+    """Insert the lines below or above a certain location in the content."""
     lines = content.splitlines()
     result_lines = []
     inserted = False
-    if method == 'below':
+    if method == "below":
         for line in lines:
             result_lines.append(line)
             if not inserted and location_keyword in line:
                 result_lines.append(insert_text)
                 inserted = True
-    elif method == 'above':
+    elif method == "above":
         for line in lines:
             if not inserted and location_keyword in line:
                 result_lines.append(insert_text)
@@ -112,15 +120,17 @@ def recompose_manuscript(manuscript_path, user_packages, user_commands):
 
 def get_bib_path_type(bib_path):
     if bib_path == "None":
-        return 'none'
+        return "none"
     if bib_path.startswith("https:"):
         return "url"
     else:
         bib_path = Path(bib_path).expanduser()
         if not bib_path.exists():
-            raise FileExistsError(f"Couldn't find {str(bib_path)}. "
-                                  "Please try again after running "
-                                  f"'touch {str(bib_path)}'.")
+            raise FileExistsError(
+                f"Couldn't find {str(bib_path)}. "
+                "Please try again after running "
+                f"'touch {str(bib_path)}'."
+            )
         else:
             return "local"
 
@@ -130,7 +140,7 @@ def copy_bib_from_url(bib_path, project_dir):
     with tempfile.TemporaryDirectory() as tmp:
         tmp_path = Path(tmp)
         subprocess.run(["git", "clone", bib_path, str(tmp_path)], check=True)
-        for item in tmp_path.glob('**/*'):
+        for item in tmp_path.glob("**/*"):
             if item.is_file() and str(item).endswith(".bib"):
                 bib_names.append(item.name)
                 dest = project_dir / item.name
@@ -142,7 +152,7 @@ def copy_bib_from_local(bib_path, project_dir):
     bib_names = []
     bib_path = Path(bib_path).expanduser()
     if bib_path.is_dir():
-        for item in bib_path.glob('**/*'):
+        for item in bib_path.glob("**/*"):
             if item.is_file() and str(item).endswith(".bib"):
                 bib_names.append(item.name)
                 dest = project_dir / item.name
@@ -155,10 +165,10 @@ def copy_bib_from_local(bib_path, project_dir):
 
 
 def insert_headers_from_repo(project_dir, manuscript_path, headers_repo_url):
-    if ( headers_repo_url ==
-            "use-scikit-package-default"):
-        user_headers_repo_url = \
+    if headers_repo_url == "use-scikit-package-default":
+        user_headers_repo_url = (
             "https://github.com/scikit-package/default-latex-headers.git"
+        )
     else:
         user_headers_repo_url = "{{ cookiecutter.latex_headers_repo_url }}"
     user_headers = get_user_headers(user_headers_repo_url)
@@ -170,17 +180,26 @@ def insert_headers_from_repo(project_dir, manuscript_path, headers_repo_url):
 
 def insert_bibliography_from_path(project_dir, manuscript_path, bib_path):
     bib_path_type = get_bib_path_type(bib_path)
-    if bib_path_type == 'url':
+    if bib_path_type == "url":
         bib_names = copy_bib_from_url(bib_path, project_dir)
-    elif bib_path_type == 'local':
+    elif bib_path_type == "local":
         bib_names = copy_bib_from_local(bib_path, project_dir)
     else:
         return
-    insert_bibliography = r'\bibliography{' + ", ".join(bib_names) + r'}'
-    manuscript_bibliography = extract_manuscript_keyword_lines(manuscript_path, keyword=r"\bibliography")
-    all_bibliography = '\n'.join([insert_bibliography, manuscript_bibliography])
+    insert_bibliography = r"\bibliography{" + ", ".join(bib_names) + r"}"
+    manuscript_bibliography = extract_manuscript_keyword_lines(
+        manuscript_path, keyword=r"\bibliography"
+    )
+    all_bibliography = "\n".join(
+        [insert_bibliography, manuscript_bibliography]
+    )
     manuscript_contents = manuscript_path.read_text(encoding="utf-8")
-    manuscript_content_with_bibliography = insert_keyword_lines(manuscript_contents, all_bibliography, location_keyword=r'\end{document}', method='above')
+    manuscript_content_with_bibliography = insert_keyword_lines(
+        manuscript_contents,
+        all_bibliography,
+        location_keyword=r"\end{document}",
+        method="above",
+    )
     manuscript_path.write_text(
         manuscript_content_with_bibliography, encoding="utf-8"
     )
