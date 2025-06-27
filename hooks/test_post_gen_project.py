f51ffc3a9af08458285f93fac3444675b357e9ec
Yuchen Ethan Xiao <yx2924@columbia.edu>
2025-06-27 19:38:49 -0400

    refactor: rename the variable in `test_copy_all_files`

--- hooks/test_post_gen_project.py
+++ hooks/test_post_gen_project.py
@@ -61,43 +61,43 @@ def test_copy_all_files(user_filesystem, user_repo_files_and_contents):
         assert file_path.read_text() == value
 
 
-# C1: An nonexistent source dir and an existing target dir.
+# C1: An non-existing source dir and an existing target dir.
 #  Expect FileNotFoundError.
 # C2: An empty source dir and an existing target dir.
 #  Expect FlileNotFoundError.
-# C3: Existing source dir and target dir, but a file exists in both dir.
-#  Expect FileExistsError.
+# C3: Existing source dir and target dir,  there is a file with the same name
+#  found in both dirs. Expect FileExistsError.
 def test_copy_all_files_bad(user_filesystem):
-    # nonexistent source directory
-    source_dir = user_filesystem / "other-dir"
-    assert not source_dir.exists()
+    # non-existing source directory
+    non_existing_source_dir = user_filesystem / "other-dir"
+    assert not non_existing_source_dir.exists()
     target_dir = user_filesystem / "target-dir"
     with pytest.raises(
         FileNotFoundError,
         match="Unable to find the source directory: "
-        f"{str(source_dir)}. Please leave an issue "
+        f"{str(non_existing_source_dir)}. Please leave an issue "
         "on GitHub.",
     ):
-        copy_all_files(source_dir, target_dir)
+        copy_all_files(non_existing_source_dir, target_dir)
 
     # empty source directory
-    source_dir = user_filesystem / "empty-dir"
-    assert source_dir.exists() and (not any(source_dir.iterdir()))
+    empty_source_dir = user_filesystem / "empty-dir"
+    assert empty_source_dir.exists() and (not any(empty_source_dir.iterdir()))
     with pytest.raises(
         FileNotFoundError,
-        match=f"Source directory {str(source_dir)} found "
+        match=f"Source directory {str(empty_source_dir)} found "
         "but it contains no files. Please leave an issue "
         "on GitHub.",
     ):
-        copy_all_files(source_dir, target_dir)
+        copy_all_files(empty_source_dir, target_dir)
 
-    # target directory with duplicated file
-    source_dir = user_filesystem / "source-dir"
+    # a file with the same name found in both dirs.
+    gh_repo_dir = user_filesystem / "source-dir"
     dir_with_duplicated_file = user_filesystem / "duplicated-dir"
-    source_file = source_dir / "usepackage.txt"
+    source_file = gh_repo_dir / "usepackage.txt"
     dest_file = dir_with_duplicated_file / "usepackage.txt"
     assert (
-        source_dir.exists()
+        gh_repo_dir.exists()
         and dir_with_duplicated_file.exists()
         and source_file.exists()
         and dest_file.exists()
@@ -110,4 +110,4 @@ def test_copy_all_files_bad(user_filesystem):
         "or leave an issue on GitHub if you think the problem is with "
         "scikit-package.",
     ):
-        copy_all_files(source_dir, dir_with_duplicated_file)
+        copy_all_files(gh_repo_dir, dir_with_duplicated_file)
